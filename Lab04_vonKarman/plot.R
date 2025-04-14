d75dd480815808208198017b730f60a6e0f89458
David Kahler <david.m.kahler@gmail.com>
2025-04-14 16:40:23 -0400

    update vortex shedding

--- Lab04_vonKarman/plot.R
+++ Lab04_vonKarman/plot.R
@@ -8,18 +8,53 @@ library(latex2exp)
 
 x <- read_csv("Lab04_vonKarman/vec20250412122224.csv", col_names = FALSE)
 x <- rename(x, time=X2, u=X5, v=X6, w1=X7, w2=X8)
+recordFrequency <- 60 # from configuration information
 
+begin <- 10
+finish <- 20
 y <- x %>%
-     filter(time>10) %>%
-     filter(time<=20) %>%
+     filter(time > begin) %>%
+     filter(time <= finish) %>%
      select(time, u, v, w1, w2)
 
 x_mean = mean(y$u)
+print(paste0("Mean streamwise velocity: ", x_mean, " m/s"))
 
 ggplot(y) +
      geom_line(aes(x=time,y=v)) +
-     theme(panel.background = element_rect(fill = "white", colour = "black")) +
+     ylim(c(-0.5, 0.5)) +
      labs(x = "Time (s)", y = "Velocity (m/s)") +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) +
      theme(aspect.ratio = 1) +
      theme(axis.text = element_text(face = "plain", size = 14)) +
      theme(axis.title = element_text(face = "plain", size = 14))
+
+v_freq  <- Mod(fft(y$v))
+freq <- (seq(0,(length(v_freq)-1))) / (finish - begin)
+v_freq <- data.frame(freq, v_freq) %>%
+     rename(Frequency = freq, `FFT` = v_freq)
+
+ggplot(v_freq) +
+     geom_line(aes(x=Frequency,y=`FFT`)) +
+     xlim(c(0,10)) +
+     theme(panel.background = element_rect(fill = "white", colour = "black")) +
+     theme(aspect.ratio = 1) +
+     theme(axis.text = element_text(face = "plain", size = 14)) +
+     theme(axis.title = element_text(face = "plain", size = 14))
+
+# FFT example
+# Do not run
+t <- seq(0,10, by=(1/60))
+s <- sin(t*2*pi)
+plot(t,s)
+f <- Mod(fft(s))
+plot(f, xlim=c(0,20))
+
+t <- seq(0,10, by=(1/60))
+s <- sin(t*2*pi*2.4)
+plot(t,s)
+f <- Mod(fft(s))
+plot(f, xlim=c(0,100))
+
+
+
